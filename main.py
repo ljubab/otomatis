b9b78d88fc69000267afba6467b513916b7b8090
Ljubomir Banovic <ljuba.banovic@gmail.com>
2024-09-17 16:55:47 +0200

    pong on post request

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
 import socketserver
-from http.server import SimpleHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler
+
+class HandleRequest(BaseHTTPRequestHandler):
+    def do_POST(self):
+        print("pong!")
+        self.send_response(200)
 
 PORT = 10045
 
-with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
+with socketserver.TCPServer(("", PORT), HandleRequest) as httpd:
     print("It works")
     httpd.serve_forever()
