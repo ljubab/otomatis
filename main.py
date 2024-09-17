231f475dcf5ea0406d9100c19a8f42838ab95e35
Ljubomir Banovic <ljuba.banovic@gmail.com>
2024-09-17 21:41:33 +0200

    create test case IO files on post request

--- main.py
+++ main.py
@@ -2,11 +2,30 @@
 
 import socketserver
 from http.server import BaseHTTPRequestHandler
+import json
+import os
 
 class HandleRequest(BaseHTTPRequestHandler):
     def do_POST(self):
-        print("pong!")
-        self.send_response(200)
+        content_length = int(self.headers['Content-Length'])
+        post_data = json.loads(self.rfile.read(content_length))
+        test_cases = post_data['tests']
+
+        test_case_directory = f'{os.getcwd()}/testCases'
+        os.mkdir(test_case_directory, 0o777)
+
+        test_case_count = 0
+        for test_case in test_cases:
+            test_case_count += 1
+
+            test_case_input_location = f'{test_case_directory}/{test_case_count}.in'
+            with open(test_case_input_location, 'w') as f:
+                f.write(test_case['input'])
+
+            test_case_output_location = f'{test_case_directory}/{test_case_count}.out'
+            with open(test_case_output_location, 'w') as f:
+                f.write(test_case['output'])
+
 
 PORT = 10045
 
